8d159cde3a8a2670e400eef98720b31fd5c8fca9
Haochen Fa <billy.fa@qq.com>
2025-09-25 08:15:36 +0800

    Chore (CircleShape): Add more details

--- src/hk/edu/polyu/comp/comp2021/clevis/model/CircleShape.java
+++ src/hk/edu/polyu/comp/comp2021/clevis/model/CircleShape.java
@@ -1,3 +1,10 @@
+/**
+ * @author FA, Haochen 24113347D
+ * @date_created 25th Sep, 2025
+ * @latest_update N/A
+ * @Description Circle Class.
+ */
+
 package hk.edu.polyu.comp.comp2021.clevis.model;
 
 /**
@@ -8,6 +15,14 @@ public final class CircleShape extends AbstractShape {
     private double centerY;
     private final double radius;
 
+    /**
+     * @constructor
+     * 
+     * @param name
+     * @param centerX Center `x` coordinate
+     * @param centerY Center `y` coordinate
+     * @param radius
+     */
     public CircleShape(String name, double centerX, double centerY, double radius) {
         super(name);
         if (radius <= 0) {
@@ -18,6 +33,11 @@ public final class CircleShape extends AbstractShape {
         this.radius = radius;
     }
 
+    /**
+     * @function `getBoundingBox`
+     * 
+     * @return A BBox that encloses the circle
+     */
     @Override
     public BoundingBox getBoundingBox() {
         return BoundingBox.of(centerX - radius, centerY - radius, centerX + radius, centerY + radius);
@@ -29,14 +49,24 @@ public final class CircleShape extends AbstractShape {
         centerY += dy;
     }
 
+    /**
+     * @function `covers` Checks whether a pint lies on the circumference w/n a
+     *           tolerance (epsilon) by computing the Euc. distance from the center
+     *           and comparing it to radius
+     * 
+     * @param point   The point candidate
+     * @param epsilon The tolerance value
+     * @return
+     */
     @Override
-    public boolean covers(Point2D point, double tolerance) {
+    public boolean covers(Point2D point, double epsilon) {
         double dx = point.getX() - centerX;
         double dy = point.getY() - centerY;
         double distance = Math.hypot(dx, dy);
-        return Math.abs(distance - radius) < tolerance;
+        return Math.abs(distance - radius) < epsilon;
     }
 
+    // @accessors
     public double getCenterX() {
         return centerX;
     }
